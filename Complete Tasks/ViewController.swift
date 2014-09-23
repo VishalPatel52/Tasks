64c23534dfe7041a7d3338d2aa3563d12f04199b
Vishal Patel <vishalpatel52@gmail.com>
2014-09-23 15:31:57 +0200

    Initial Commit

---
+++ Complete Tasks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Complete Tasks
+//
+//  Created by Vishal Patel on 23/09/14.
+//  Copyright (c) 2014 Vishal Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
